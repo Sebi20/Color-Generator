0d7c6d2f542a16754c4e2e53b7eac301091d1c86
Sebastian <sebastian.stephenson1@gmail.com>
2022-07-12 09:21:17 -0400

    First commit, just completed the base

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
+  function renderColor(){
+
+  }
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Random Color Generator</h1>
+
+    <button>Random color</button>
+
+    <div className='colorboard'></div>
+    </>
   );
 }
 
