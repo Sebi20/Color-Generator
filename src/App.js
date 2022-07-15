780637a9262ccc148c4a38749847946335fad747
Sebastian <sebastian.stephenson1@gmail.com>
2022-07-14 22:43:39 -0400

    Made the function that picks a random rgb color

--- src/App.js
+++ src/App.js
@@ -4,19 +4,27 @@ function App() {
 
 
   function renderColor(){
-
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    document.getElementById("colorboard").style.backgroundColor= 'rgb(' + r + ',' + g + ',' + b + ')';
   }
+
+  
 
   
   return (
     <>
     <h1>Random Color Generator</h1>
 
-    <button>Random color</button>
+    <button onClick={renderColor}>Random color</button>
 
-    <div className='colorboard'></div>
+    <div id='colorboard'></div>
+
     </>
   );
+
+ 
 }
 
 export default App;
