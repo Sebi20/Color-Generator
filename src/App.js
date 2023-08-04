e37c1ca762561f9fd174c194768153cdef781318
Sebastian <sebastian.stephenson1@gmail.com>
2023-08-04 09:42:48 -0400

    Created the states for hex values

--- src/App.js
+++ src/App.js
@@ -3,31 +3,17 @@ import {useState} from 'react';
 import $ from 'jquery';
 
 function App() {
+
+  // RGB states
   const [r, setR] = useState(0);
   const [g, setG] = useState(0);
   const [b, setB] = useState(0);
 
+  // Hex code states
+  const [hexRed, setHexRed] = useState("");
+  const [hexGreen, setHexGreen] = useState("");
+  const [hexBlue, setHexBlue] = useState("");
 
-  function renderRandomColor(){
-     
-    setR(Math.floor(Math.random() * 256));
-    setG(Math.floor(Math.random() * 256));
-    setB(Math.floor(Math.random() * 256));
-  }
-
-  function renderRandomRed(){
-    setR(Math.floor(Math.random() * 256));
-  }
-
-  function renderRandomGreen(){
-     
-    setG(Math.floor(Math.random() * 256));
-  }
-
-  function renderRandomBlue(){
-     
-    setB(Math.floor(Math.random() * 256));
-  }
 
   $("input[name = r").val(r);
   $("input[name = g").val(g);
@@ -37,9 +23,34 @@ function App() {
   $("input[name = rangeGreen").val(g);
   $("input[name = rangeBlue").val(b);
 
+  // Function to get random color
+  function renderRandomColor(){
+    setR(Math.floor(Math.random() * 256));
+    setG(Math.floor(Math.random() * 256));
+    setB(Math.floor(Math.random() * 256));
+  }
 
+  // Function to get random red value
+  function renderRandomRed(){
+    setR(Math.floor(Math.random() * 256));
+  }
+
+  // Function to get random green value
+  function renderRandomGreen(){
+    setG(Math.floor(Math.random() * 256));
+  }
+  
+  // Functio nto get random blue value
+  function renderRandomBlue(){
+    setB(Math.floor(Math.random() * 256));
+  }
+  
+  // Function that renders out the colors after every change
   function renderColor(e){
     e.preventDefault();
+
+    // makeZeroDefault();
+    
     setR($("input[name = r").val());
     setG($("input[name = g").val());
     setB($("input[name = b").val());
@@ -52,7 +63,14 @@ function App() {
     setB($("input[name = rangeBlue").val());
 
   }
+ 
 
+  // function makeZeroDefault(){
+  //   if($("input[name = b").val() === ''){
+  //     $("#blue").text(" Exercises Solution");
+  //     console.log("here")
+  //   }
+  // }
  
   
   return (
@@ -62,11 +80,11 @@ function App() {
       
       <form>
         <label style={{color:"red"}}>R:</label>
-        <input  onChange={renderColor} id='red' name='r' type='text' defaultValue={r}/>
+        <input  onChange={renderColor} id='red' name='r' type='number' min='0' max='255' defaultValue={r}/>
         <label style={{color:"limegreen"}}>G:</label>
-        <input  onChange={renderColor} id='green' name='g' type='text' defaultValue={g}/>
+        <input  onChange={renderColor} id='green' name='g' type='number' min='0' max='255' defaultValue={g}/>
         <label style={{color:"deepskyblue"}}>B:</label>
-        <input onChange={renderColor} id='blue' name='b' type='text' defaultValue={b}/>
+        <input onChange={renderColor} id='blue' name='b' type='number' min='0' max='255' defaultValue={b}/>
       </form>
 
       <h3>-OR-</h3>
